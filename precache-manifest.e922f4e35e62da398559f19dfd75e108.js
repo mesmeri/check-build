a66243e6543e83dd2f055139ddaa42d47ec1498c
mesmeri <49590226+mesmeri@users.noreply.github.com>
2020-09-24 18:56:59 +0300

    Add files via upload

---
+++ precache-manifest.e922f4e35e62da398559f19dfd75e108.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f3fec068e28168fd5764aba9c63280",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939361d5e0b371a3b168",
+    "url": "/static/css/10.df7c4815.chunk.css"
+  },
+  {
+    "revision": "32744829b801beb69bd9",
+    "url": "/static/css/4.a2eb9472.chunk.css"
+  },
+  {
+    "revision": "6b9046819daf38e12e9d",
+    "url": "/static/css/5.c5c44ecc.chunk.css"
+  },
+  {
+    "revision": "a03f50754c19fc136cf2",
+    "url": "/static/css/6.1f43cf81.chunk.css"
+  },
+  {
+    "revision": "59e98f5063c587fcce80",
+    "url": "/static/css/7.d37ea8c5.chunk.css"
+  },
+  {
+    "revision": "e547e03b089630ec106d",
+    "url": "/static/css/9.ddba9610.chunk.css"
+  },
+  {
+    "revision": "41bc3658041d9b0efda9",
+    "url": "/static/css/main.250a1036.chunk.css"
+  },
+  {
+    "revision": "88f4394d59430a937c17",
+    "url": "/static/js/0.40f3eab6.chunk.js"
+  },
+  {
+    "revision": "939361d5e0b371a3b168",
+    "url": "/static/js/10.bbb22c60.chunk.js"
+  },
+  {
+    "revision": "903126ec2b1fa72f97f8",
+    "url": "/static/js/3.2868067c.chunk.js"
+  },
+  {
+    "revision": "f75feb5ff2f098d169d08fee761fb5d9",
+    "url": "/static/js/3.2868067c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32744829b801beb69bd9",
+    "url": "/static/js/4.0842d1fa.chunk.js"
+  },
+  {
+    "revision": "6b9046819daf38e12e9d",
+    "url": "/static/js/5.54ceecae.chunk.js"
+  },
+  {
+    "revision": "a03f50754c19fc136cf2",
+    "url": "/static/js/6.3de3d03d.chunk.js"
+  },
+  {
+    "revision": "59e98f5063c587fcce80",
+    "url": "/static/js/7.17d3ecb1.chunk.js"
+  },
+  {
+    "revision": "f13787c50ce4dde9b936",
+    "url": "/static/js/8.c00dd1b2.chunk.js"
+  },
+  {
+    "revision": "e547e03b089630ec106d",
+    "url": "/static/js/9.eb4a096f.chunk.js"
+  },
+  {
+    "revision": "41bc3658041d9b0efda9",
+    "url": "/static/js/main.0b47d222.chunk.js"
+  },
+  {
+    "revision": "3185abfaa38bb9ac59cb",
+    "url": "/static/js/runtime-main.ac0296f2.js"
+  },
+  {
+    "revision": "87c5dcac9cce7a1adce89c46994bf5b6",
+    "url": "/static/media/advanced-js-bg.87c5dcac.jpg"
+  },
+  {
+    "revision": "92753e24293abb68a2e4d8f012e1bb1a",
+    "url": "/static/media/basic-js-bg.92753e24.jpg"
+  },
+  {
+    "revision": "a3a970e9ed24be3f2b3f93882c1fd183",
+    "url": "/static/media/start-page-bg.a3a970e9.jpg"
+  }
+]);
